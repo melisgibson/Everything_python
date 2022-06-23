36160f52c51ed6ae9497618e48a0737936d0b645
EC2 Default User <ec2-user@ip-172-31-77-66.ec2.internal>
2022-06-23 19:59:00 +0000

    completed

--- aws_services_list.py
+++ aws_services_list.py
@@ -12,3 +12,24 @@ print(services_list)
 services_list.append("Athena")
 print(services_list)
 
+# insert item into 2nd place on the list
+services_list.insert(1, "CloudFront")
+print(services_list)
+
+# print length of the list
+print(len(services_list))
+
+# remove a service from the list by name
+services_list.remove("DynamoDB")
+print(services_list)
+
+# remove a service from the list by index, the 4th item 
+del services_list[3]
+print(services_list)
+
+# print the new list and the new length of the list
+print(services_list)
+print(len(services_list))
+
+# print a sorted list
+print(sorted(services_list))
