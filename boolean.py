ef7a8d23067355cd174daa94a18ec11fc8177a31
EC2 Default User <ec2-user@ip-172-31-77-66.ec2.internal>
2022-06-23 18:47:27 +0000

    boolean

---
+++ boolean.py
@@ -0,0 +1,12 @@
+# opposite of True
+print(not True)
+
+# OR operator, if either is true then it is true
+print(True or True)
+print(True or False)
+print(False or False)
+
+# AND operator, both must be true to be true
+print(True and True)
+print(True and False)
+print(False and False)
