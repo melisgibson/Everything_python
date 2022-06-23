ccbcccb74da6bdabc969826bc7e09e99de576919
EC2 Default User <ec2-user@ip-172-31-77-66.ec2.internal>
2022-06-23 18:14:42 +0000

    bitwise

--- bitwise.py
+++ bitwise.py
@@ -1,11 +1,24 @@
 # binary numbers
 a = 0b1001
 b = 0b1100
+
+# prints the decimal equivalent
+print(a)
+
+# prints the binary 
+print(bin(a)) 
+
 # bitwise OR operator
 print(bin(a | b))
+
 # bitwise AND operator
 print(bin(a & b))
+
 # bitwise XOR operator
 print(bin(a ^ b))
-# shift operator
+
+# shift operator right shift
 print(bin(a >> 2))
+
+#shift operator left shift
+print(bin(a << 4))
