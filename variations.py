42e142e8821fe1d0a9d5e1102454fcbea5da00d1
EC2 Default User <ec2-user@ip-172-31-77-66.ec2.internal>
2022-06-25 19:24:45 +0000

    variations

---
+++ variations.py
@@ -0,0 +1,17 @@
+# User input a message
+message = input("Enter a message: ")
+
+# prints the message in different variations
+print("Lowercase:", message.lower())
+print("Uppercase:", message.upper())
+print("Capitalized:", message.capitalize())
+print("Title Case:", message.title())
+
+# splits up the words
+words = message.split()
+print("Words:", words)
+
+# sorts first word and last word
+sorted_words = sorted(words)
+print("Alphabetic First Word:", sorted_words[0])
+print("Alphabetic Last Word:", sorted_words[-1])
