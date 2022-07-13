d6982f0a965f954a46a9076149f3b040d8306dee
melisgibson <melcassidy45@gmail.com>
2022-07-13 01:12:01 +0000

    lambda back up ec2 instances

--- connect_cloud9_dynamodb.py
+++ connect_cloud9_dynamodb.py
@@ -1,11 +1,10 @@
 import boto3
 
-# connect to dynamodb
+# Connect to dynamodb
 
 dynamodb = boto3.resource(
     'dynamodb',
-    aws_access_key_id='xxxxxxxxxxxx',
-    aws_secret_access_key='xxxxxxxxxxxxxxxx',
+    aws_access_key_id='AKIAQMYWJSHVI5OBGKS6', # Input access key id
+    aws_secret_access_key='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', # Input secret key
     )
-    
     
