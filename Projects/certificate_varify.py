5d8eb06635b183691ce2b2553190424c4143381e
CoinGraham-ClearData <99690697+CoinGraham-ClearData@users.noreply.github.com>
2022-11-22 14:13:41 -0600

    updates to certificate verify

--- Projects/certificate_varify.py
+++ Projects/certificate_varify.py
@@ -1,29 +1,29 @@
-from urllib.request import ssl, socket
-import datetime, smtplib
+import ssl
+import socket
+import datetime
 
 print("Program to check SSL certificate validity and expiration date \n")
 
-##opening file 
-ipfile=open('server_ip.txt')
+# opening file
+with open('server_ip.txt') as ip_file:
 
-##check  certificate expiration
-for ip in ipfile:
- try:
-   host = ip.strip().split(":")[0]
-   port = ip.strip().split(":")[1]
-   print("\nChecking certifcate for server ",host)
-   context = ssl.create_default_context()
-   with socket.create_connection((host, port)) as sock:
-    with context.wrap_socket(sock, server_hostname=host) as ssock:
-        certificate = ssock.getpeercert()
-        print("Version: ",ssock.version())
-    certExpires = datetime.datetime.strptime(certificate['notAfter'], '%b %d %H:%M:%S %Y %Z')
-    daysToExpiration = (certExpires - datetime.datetime.now()).days
-    print ("Expires on: ",certExpires)
-    print ("In ",daysToExpiration," days")  
+    # check  certificate expiration
+    for ip in ip_file:
+        try:
+            host = ip.strip().split(":")[0]
+            port = int(ip.strip().split(":")[1])
+            print("\nChecking certificate for {}".format(host))
+            context = ssl.create_default_context()
+            with socket.create_connection((host, port)) as sock:
+                with context.wrap_socket(sock, server_hostname=host) as ssock:
+                    certificate = ssock.getpeercert()
+                    print("Version: {}".format(ssock.version()))
+                certExpires = datetime.datetime.strptime(certificate['notAfter'], '%b %d %H:%M:%S %Y %Z')
+                daysToExpiration = (certExpires - datetime.datetime.now()).days
+                print("Expires on: {}".format(certExpires))
+                print("In {} days".format(daysToExpiration))
 
- except:
-   print ("error on connection to Server,",host)
+        except Exception as e:
+            print("Unknown error occurred {}.".format(e))
 
-         
-print ('\nCert check complete!')
+print("\nCert check complete!")
