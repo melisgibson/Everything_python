021a5a8aac9ae1e73e2cb51d3a520d7b381e4584
melisgibson <melcassidy45@gmail.com>
2022-11-23 09:55:31 -0600

    update

--- Projects/certificate_varify.py
+++ Projects/certificate_varify.py
@@ -1,31 +1,29 @@
-from urllib.request import ssl, socket
-import datetime, smtplib
+import ssl
+import socket
+import datetime
 
 print(f"Program to check SSL certificate validity and expiration date\n")
 
 ##opening file
-ipfile = open("server_ip.txt")
+with open("server_ip.txt") as ip_file:
 
-##check  certificate expiration
-for ip in ipfile:
-    try:
-        host = ip.strip().split(":")[0]
-        port = ip.strip().split(":")[1]
-        print(f"\nChecking certifcate for server {host}")
-        context = ssl.create_default_context()
-        with socket.create_connection((host, port)) as sock:
-            with context.wrap_socket(sock, server_hostname=host) as ssock:
-                certificate = ssock.getpeercert()
-                print(f"Version: {ssock.version()}")
-            certExpires = datetime.datetime.strptime(
-                certificate["notAfter"], "%b %d %H:%M:%S %Y %Z"
-            )
-            daysToExpiration = (certExpires - datetime.datetime.now()).days
-            print(f"Expires on: {certExpires}")
-            print(f"In {daysToExpiration} days")
+    ##check  certificate expiration
+    for ip in ip_file:
+        try:
+            host, port = ip.strip().split(":")
+            print(f"\nChecking certifcate for server {host}")
+            context = ssl.create_default_context()
+            with socket.create_connection((host, port)) as sock:
+                with context.wrap_socket(sock, server_hostname=host) as ssock:
+                    certificate = ssock.getpeercert()
+                certExpires = datetime.datetime.strptime(
+                    certificate["notAfter"], "%b %d %H:%M:%S %Y %Z"
+                )
+                daysToExpiration = (certExpires - datetime.datetime.now()).days
+                print(f"Expires on: {certExpires} in {daysToExpiration} days")
 
-    except:
-        print(f"error on connection to Server, {host}")
+        except:
+            print(f"error on connection to Server, {host}")
 
 
 print(f"\nCert check complete!")
